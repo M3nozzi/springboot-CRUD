0f92719b7462529a70b3f6234d4833e17f051a05
Fabio Menozzi <f.menozzi@yahoo.com>
2020-10-05 16:06:50 -0300

    just beginning

---
+++ src/main/java/com/menozzi/wkmongo/WkmongoApplication.java
@@ -0,0 +1,13 @@
+package com.menozzi.wkmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menozzi/wkmongo/WkmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menozzi.wkmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
