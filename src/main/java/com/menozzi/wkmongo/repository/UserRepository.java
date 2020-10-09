83776e8f51ca407ff7762e57a279eef8a044fdf1
Fabio Menozzi <f.menozzi@yahoo.com>
2020-10-09 12:40:51 -0300

    Delete user working

--- src/main/java/com/menozzi/wkmongo/repository/UserRepository.java
+++ src/main/java/com/menozzi/wkmongo/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.menozzi.wkmongo.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.menozzi.wkmongo.domain.User;
@@ -10,7 +11,6 @@ import com.menozzi.wkmongo.domain.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
-	void save(List<User> asList);
 
 	
 }
